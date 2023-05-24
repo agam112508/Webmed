9ce49659052facc3dd65a73eef34ded443b36e9b
Esteban Agamez <90142518+agam112508@users.noreply.github.com>
2023-05-24 08:08:13 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+const time= document.getElementById('time');
+const date= document.getElementsById('date')
+
+const monthName = ["Enero" , "Febrero" , "Marzo" , "Abril" , "Mayo", "Junio" , "Julio" , "Agosto" , "Septiembre" , "Octubre" , "Noviembre" , "Diciembre"] 
+
+
+const interval = setInterval (() => {
+
+    const local = new Date();
+
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear;
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthName[month]} ${year}`;
+
+}, 1000);
